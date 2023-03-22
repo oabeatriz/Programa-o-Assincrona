a4b04b4f293d9c1baa7f2f13d8bfd91d67178b5d
Ana Beatriz de Oliveira Assis <107081846+oabeatriz@users.noreply.github.com>
2023-03-22 18:31:06 +0000

    .

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function novoUsuario(){
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      var erro = false;
+      var erro = true;
       if(!erro){
         resolve({ mensagem: "Sucesso."});
          }else{
@@ -13,10 +13,24 @@ function novoUsuario(){
     }, 2000)
   });
 }
-// await é um comando pro javascript aguardar a resposta da promise antes de executar o programa.
+// await é um comando pro javascript aguardar a resposta da promise antes de executar o programa. apenas para resolve!!
 async function main(){
   var dados = await novoUsuario();
   console.log(dados);
   
+}
+main();
+
+// tratando um erro utilizando async e await
+
+async function main(){
+
+try{
+   var dados = await novoUsuario();
+  console.log(dados);
+}catch(erro){
+  console.log("Erro.")
+}
+
 }
 main();
